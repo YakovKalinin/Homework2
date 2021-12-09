df940ac18033a8679eb4726b181ab022a4619b94
Kalinin Yakov <z-chutvishechem@mail.ru>
2021-12-09 18:57:16 +0700

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 12000;
+        int price = 8_000;
         int bonus = 20;
         int reward = price / bonus;
         System.out.println("Your reward is " + reward + " miles");
