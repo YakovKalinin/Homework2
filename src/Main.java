323f2093ca420f6a5c218aae7371c6f3238d3837
Kalinin Yakov <z-chutvishechem@mail.ru>
2021-12-09 08:59:48 +0700

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        int price = 12000;
+        int bonus = 20;
+        int reward = price / bonus;
+        System.out.println("Your reward is " + reward + " rub");
 
     }
 }
-gitgit
